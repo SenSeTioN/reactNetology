9b128e13b15bcaa58cfecb5653cf8a7fe0f1c5e5
SenSeTioN <lankovedik@gmail.com>
2023-11-29 22:04:53 +0300

    complete lesson 1.2

---
+++ src/components/store-class/components/ShopItemClass.tsx
@@ -0,0 +1,35 @@
+import { Component } from 'react'
+import '../css/main.css'
+
+interface IProps {
+  item: {
+    brand: string
+    title: string
+    description: string
+    descriptionFull: string
+    price: number
+    currency: string
+  }
+}
+
+export default class ShopItemClass extends Component<IProps> {
+  render() {
+    const { item } = this.props
+    return (
+      <div className='main-content'>
+        <h2>{item.brand}</h2>
+        <h1>{item.title}</h1>
+        <h3>{item.description}</h3>
+        <div className='description'>{item.descriptionFull}</div>
+        <div className='highlight-window mobile'>
+          <div className='highlight-overlay'></div>
+        </div>
+        <div className='divider'></div>
+        <div className='purchase-info'>
+          <div className='price'>{`${item.currency}${item.price}.00`}</div>
+          <button>Добавить в корзину</button>
+        </div>
+      </div>
+    )
+  }
+}
